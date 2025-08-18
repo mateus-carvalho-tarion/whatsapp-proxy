6bfaf9952c8a33113d0067646b91a134e85d6bcd
Mateus Vergílio de Carvalho <mateusvergiliocarvalho@gmail.com>
2025-08-18 15:53:26 -0300

    Axios removed from API

--- api.js
+++ api.js
@@ -1,9 +1,7 @@
-const axios = require('axios');
 const router = require('express').Router();
 
 router.post('/', async (req, res) => {
     try {
-        // await axios.post(process.env.WEBHOOK_SITE_URL + req.originalUrl, extractedData);
         console.log(req.body);
     } catch (err) {
         console.error('Error processing webhook:', err);
