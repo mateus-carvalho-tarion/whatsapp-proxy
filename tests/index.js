7b8842de1c92b156481c6021ca85ae36a5abcfd3
Mateus Vergílio de Carvalho <mateusvergiliocarvalho@gmail.com>
2025-08-18 14:10:39 -0300

    Hardcoded envs for tests

--- tests/index.js
+++ tests/index.js
@@ -1,1 +1,6 @@
+// Forced ENVs to match sample files.
+// Only for hardcoded tests, not for production use.
+process.env.ACCOUNT_ID = '110700000000000_WHATSAPP_BUSINESS_ACCOUNT_ID'
+process.env.PHONE_NUMBER_ID = '119900000000000_WHATSAPP_PHONE_NUMBER_ID'
+
 require('./utils');
