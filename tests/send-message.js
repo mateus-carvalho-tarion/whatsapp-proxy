13b64fe1aadab41cdcf3dc1f6500abac94ee2c63
Mateus Vergílio de Carvalho <mateusvergiliocarvalho@gmail.com>
2025-08-18 17:17:55 -0300

    Interactive messages

--- tests/send-message.js
+++ tests/send-message.js
@@ -1,6 +1,5 @@
 const sendMessage = require('../utils/send-message');
 
-
 (async () => {
     const recipientPhoneNumber = '5519994940824';
     let result = null;
@@ -59,6 +58,116 @@ const sendMessage = require('../utils/send-message');
     // req.params = {
     //     messageId: 'wamid.HBgNNTUxOTk5NDk0MDgyNBUCABEYEjA3NkE5RTcwNkUyNDAzMTVFMQA=',
     //     emoji: '\uD83D\uDE00' // C/C++ Java Source Code https://it-tools.tech/emoji-picker
+    // };
+
+    // Interactive CTA message with text header and footer
+    // req.type = 'interactive_cta';
+    // req.params = {
+    //     header: {
+    //         type: 'text',
+    //         text: 'Header text example'
+    //     },
+    //     body: 'Body text example',
+    //     footer: 'Footer text example',
+    //     action: {
+    //         url: 'https://www.google.com.br',
+    //         displayText: 'Open URL'
+    //     }
+    // };
+
+    // Interactive CTA message with image header and text footer
+    // req.type = 'interactive_cta';
+    // req.params = {
+    //     header: {
+    //         type: 'image',
+    //         link: 'https://file-examples.com/storage/fef6248bef689f7bb9c274f/2017/10/file_example_PNG_500kB.png'
+    //     },
+    //     body: 'Body text example',
+    //     footer: 'Footer text example',
+    //     action: {
+    //         url: 'https://www.google.com.br',
+    //         displayText: 'Open URL'
+    //     }
+    // };
+
+    // Interactive CTA message with video header and text footer
+    // req.type = 'interactive_cta';
+    // req.params = {
+    //     header: {
+    //         type: 'video',
+    //         link: 'https://file-examples.com/storage/fef6248bef689f7bb9c274f/2017/04/file_example_MP4_480_1_5MG.mp4'
+    //     },
+    //     body: 'Body text example.\nThis is a video header.',
+    //     footer: 'Footer text example',
+    //     action: {
+    //         url: 'https://www.google.com.br',
+    //         displayText: 'Open URL'
+    //     }
+    // };
+
+    // Interactive CTA message with document header and text with emoji footer
+    // req.type = 'interactive_cta';
+    // req.params = {
+    //     header: {
+    //         type: 'document',
+    //         link: 'https://file-examples.com/storage/fef6248bef689f7bb9c274f/2017/10/file-sample_150kB.pdf',
+    //         filename: 'filename-example.pdf',
+    //     },
+    //     body: 'Body text example.\nThis is a document header.',
+    //     footer: 'Footer text example \uD83D\uDE00',
+    //     action: {
+    //         url: 'https://www.google.com.br',
+    //         displayText: 'Open URL'
+    //     }
+    // };
+
+    // Interactive list message with header (only text accepted) and text footer
+    // req.type = 'interactive_list';
+    // req.params = {
+    //     header: {
+    //         type: 'text',
+    //         text: 'Sample text header'
+    //     },
+    //     body: 'Body text example',
+    //     footer: 'Footer text example',
+    //     action: {
+    //         displayText: 'Open URL',
+    //         groups: [
+    //             {
+    //                 title: 'Example group 1',
+    //                 options: [
+    //                     { id: 'option1', title: 'Option Item 1', description: 'Sample description for option item 1' },
+    //                     { id: 'option2', title: 'Option Item 2' },
+    //                 ],
+    //             },
+    //             {
+    //                 title: 'Example group 2',
+    //                 options: [
+    //                     { id: 'option1', title: 'Option Item 1 Group 2', description: 'Sample description for option item 1' },
+    //                     { id: 'option2', title: 'Option Item 2 Group 2', description: 'Sample with emoji \uD83D\uDE00' },
+    //                     { id: 'option3', title: 'Option Item 3 Group 2', description: 'Sample description for option item 1. This is a larger Option.' },
+    //                 ],
+    //             },
+    //         ],
+    //     }
+    // };
+
+    // Interactive buttons message with text header and text footer
+    // req.type = 'interactive_buttons';
+    // req.params = {
+    //     header: {
+    //         type: 'text',
+    //         text: 'Sample text header'
+    //     },
+    //     body: 'Body text example',
+    //     footer: 'Footer text example',
+    //     action: {
+    //         buttons: [
+    //             { id: 'option1', title: 'Button option 1' },
+    //             { id: 'option2', title: 'Button Item 2' },
+    //             { id: 'option3', title: 'Button Item 3' },
+    //         ]
+    //     }
     // };
 
     result = await sendMessage.sendMessage(req);
--- utils/send-message.js
+++ utils/send-message.js
@@ -32,6 +32,11 @@ module.exports.sendMessage = async ({
         case 'reaction':
             return await this.sendReactionMessage(recipientPhoneNumber, params);
 
+        case 'interactive_cta':
+        case 'interactive_list':
+        case 'interactive_buttons':
+            return await this.sendInteractiveMessage(recipientPhoneNumber, type, params);
+
         default:
             throw new Error(`Unsupported message type: ${type}`);
     }
@@ -208,4 +213,174 @@ module.exports.sendReactionMessage = async (recipientPhoneNumber = '', params = 
     }
 
     return await executeRequest(req);
+}
+
+/**
+ * https://developers.facebook.com/docs/whatsapp/cloud-api/messages/interactive-cta-url-messages
+ * https://developers.facebook.com/docs/whatsapp/cloud-api/messages/interactive-list-messages
+ * https://developers.facebook.com/docs/whatsapp/cloud-api/messages/interactive-reply-buttons-messages
+ */
+module.exports.sendInteractiveMessage = async (recipientPhoneNumber = '', type, params = {}) => {
+    if (!params.body) throw new Error('Missing "body" parameter for interactive message');
+    if (params.body.length > 4096) throw new Error('Body text must be 4096 characters or less');
+    if (!params.action) throw new Error('Missing "action" parameter for interactive message');
+    if (params.footer && params.footer.length > 60) throw new Error('Footer text must be 60 characters or less');
+
+    let req = {
+        messaging_product: "whatsapp",
+        recipient_type: "individual",
+        to: recipientPhoneNumber,
+        type: "interactive",
+        interactive: {
+            body: {
+                text: params.body,
+            },
+            action: {}
+        },
+    };
+
+    if (params.header?.type) req.interactive.header = getInteractiveHeader(params.header);
+    if (params.footer) req.interactive.footer = { text: params.footer };
+
+    switch (type) {
+        case 'interactive_cta':
+            req.interactive.type = 'cta_url';
+            req.interactive.action = getInteractiveCTAAction(params.action);
+            break;
+
+        case 'interactive_list':
+            if (params.header?.type !== 'text') throw new Error('Interactive list messages must have a text header');
+            req.interactive.type = 'list';
+            req.interactive.action = getInteractiveListAction(params.action);
+            break;
+
+        case 'interactive_buttons':
+            req.interactive.type = 'button';
+            req.interactive.action = getInteractiveButtonsAction(params.action);
+            break;
+
+        default:
+            throw new Error(`Unsupported interactive message type: ${type}`);
+    }
+
+    return await executeRequest(req);
+}
+
+function getInteractiveHeader(header = { type: 'text' }) {
+    if (!header) return null;
+
+    switch (header.type) {
+        case 'document':
+            if (!header.link) throw new Error('Missing "header.link" parameter for document header');
+            return {
+                type: "document",
+                document: {
+                    link: header.link,
+                    filename: header.filename || '',
+                }
+            };
+
+        case 'image':
+            if (!header.link) throw new Error('Missing "header.link" parameter for image header');
+            return {
+                type: "image",
+                image: {
+                    link: header.link
+                }
+            };
+
+        case 'text':
+            if (!header.text) throw new Error('Missing "header.text" parameter for text header');
+            if (header.text.length > 60) throw new Error('Header text must be 60 characters or less');
+            return {
+                type: "text",
+                text: header.text || ''
+            };
+
+        case 'video':
+            if (!header.link) throw new Error('Missing "header.link" parameter for video header');
+            return {
+                type: "video",
+                video: {
+                    link: header.link
+                }
+            };
+
+        default:
+            throw new Error(`Unsupported header type: ${header.type}`);
+    }
+}
+
+/**
+ * https://developers.facebook.com/docs/whatsapp/cloud-api/messages/interactive-cta-url-messages
+ */
+function getInteractiveCTAAction(action = {}) {
+    if (!action.displayText) throw new Error('Missing "displayText" parameter for interactive CTA action');
+    if (!action.url) throw new Error('Missing "url" parameter for interactive CTA action');
+    if (action.displayText.length > 20) throw new Error('Display text must be 20 characters or less');
+
+    return {
+        name: "cta_url",
+        parameters: {
+            display_text: action.displayText,
+            url: action.url
+        }
+    }
+}
+
+/**
+ * https://developers.facebook.com/docs/whatsapp/cloud-api/messages/interactive-list-messages
+ */
+function getInteractiveListAction(action = {}) {
+    if (!action.displayText) throw new Error('Missing "displayText" parameter for interactive list message');
+    if (!action.groups || !Array.isArray(action.groups) || action.groups.length === 0) throw new Error('Missing "groups" parameter for interactive list message');
+
+    return {
+        "button": action.displayText,
+        "sections": action.groups.map(g => {
+            if (!g.title) throw new Error('Missing "title" parameter for interactive list group');
+            if (g.title.length > 24) throw new Error('Title text must be 24 characters or less');
+            if (!g.options || !Array.isArray(g.options) || g.options.length === 0) throw new Error('Missing "options" parameter for interactive list group');
+
+            return {
+                "title": g.title,
+                "rows": g.options.map(o => {
+                    if (!o.id) throw new Error('Missing "id" parameter for interactive list option');
+                    if (o.id.length > 200) throw new Error('ID text must be 200 characters or less');
+                    if (!o.title) throw new Error('Missing "title" parameter for interactive list option');
+                    if (o.title.length > 24) throw new Error('Title text must be 24 characters or less');
+                    if (o.description && o.description.length > 72) throw new Error('Description text must be 72 characters or less');
+
+                    return {
+                        "id": o.id,
+                        "title": o.title,
+                        "description": o.description || ''
+                    }
+                })
+            }
+        })
+    };
+}
+
+/**
+ * https://developers.facebook.com/docs/whatsapp/cloud-api/messages/interactive-reply-buttons-messages
+ */
+function getInteractiveButtonsAction(action = {}) {
+    if (!action.buttons || !Array.isArray(action.buttons) || action.buttons.length === 0) throw new Error('Missing "buttons" parameter for interactive reply buttons message');
+
+    return {
+        buttons: action.buttons.map(b => {
+            if (!b.id) throw new Error('Missing "id" parameter for reply button');
+            if (b.id.length > 200) throw new Error('ID text must be 200 characters or less');
+            if (!b.title) throw new Error('Missing "title" parameter for reply button');
+            if (b.title.length > 20) throw new Error('Title text must be 20 characters or less');
+            return {
+                type: "reply",
+                reply: {
+                    id: b.id,
+                    title: b.title
+                }
+            }
+        })
+    }
 }
