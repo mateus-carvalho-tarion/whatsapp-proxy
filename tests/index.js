38e977bf2a3ba1505c109d663ce918328f252e9f
Mateus Vergílio de Carvalho <mateusvergiliocarvalho@gmail.com>
2025-08-18 15:52:25 -0300

    Utils test name refactor.

--- tests/index.js
+++ tests/index.js
@@ -3,4 +3,4 @@
 process.env.ACCOUNT_ID = '110700000000000_WHATSAPP_BUSINESS_ACCOUNT_ID'
 process.env.PHONE_NUMBER_ID = '119900000000000_WHATSAPP_PHONE_NUMBER_ID'
 
-require('./utils');
+require('./extract-data');
